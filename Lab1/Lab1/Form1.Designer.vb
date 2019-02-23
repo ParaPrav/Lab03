572db8e43834ad8d956b5578d3b29135bf766b8a
ParaPrav <47909182+ParaPrav@users.noreply.github.com>
2019-02-22 23:22:03 -0500

    Add files via upload

---
+++ Lab1/Lab1/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.AverageUnitsShippedTips = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtUnitShipped = New System.Windows.Forms.TextBox()
+        Me.lblUnitShippedPrompt = New System.Windows.Forms.Label()
+        Me.lblRecord = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnEnter.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnEnter.Location = New System.Drawing.Point(12, 218)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "&Enter"
+        Me.AverageUnitsShippedTips.SetToolTip(Me.btnEnter, "Enter to continue")
+        Me.btnEnter.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnReset.Location = New System.Drawing.Point(105, 218)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.AverageUnitsShippedTips.SetToolTip(Me.btnReset, "Clear all of the inputs")
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnExit.Location = New System.Drawing.Point(195, 218)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.AverageUnitsShippedTips.SetToolTip(Me.btnExit, "Quit the program")
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'txtUnitShipped
+        '
+        Me.txtUnitShipped.Location = New System.Drawing.Point(45, 12)
+        Me.txtUnitShipped.Name = "txtUnitShipped"
+        Me.txtUnitShipped.Size = New System.Drawing.Size(75, 22)
+        Me.txtUnitShipped.TabIndex = 1
+        Me.AverageUnitsShippedTips.SetToolTip(Me.txtUnitShipped, "input the # of Units Shipped")
+        '
+        'lblUnitShippedPrompt
+        '
+        Me.lblUnitShippedPrompt.Location = New System.Drawing.Point(11, 13)
+        Me.lblUnitShippedPrompt.Name = "lblUnitShippedPrompt"
+        Me.lblUnitShippedPrompt.Size = New System.Drawing.Size(38, 23)
+        Me.lblUnitShippedPrompt.TabIndex = 0
+        Me.lblUnitShippedPrompt.Text = "&Unit:"
+        Me.lblUnitShippedPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblRecord
+        '
+        Me.lblRecord.BackColor = System.Drawing.SystemColors.Window
+        Me.lblRecord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRecord.Location = New System.Drawing.Point(12, 48)
+        Me.lblRecord.Name = "lblRecord"
+        Me.lblRecord.Size = New System.Drawing.Size(258, 122)
+        Me.lblRecord.TabIndex = 3
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAverage.Location = New System.Drawing.Point(12, 182)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(258, 23)
+        Me.lblAverage.TabIndex = 4
+        Me.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblDay
+        '
+        Me.lblDay.Location = New System.Drawing.Point(219, 23)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(51, 23)
+        Me.lblDay.TabIndex = 2
+        Me.lblDay.Text = "Day 1"
+        Me.lblDay.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'frmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.lblRecord)
+        Me.Controls.Add(Me.txtUnitShipped)
+        Me.Controls.Add(Me.lblUnitShippedPrompt)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageUnitsShipped"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AverageUnitsShippedTips As ToolTip
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblUnitShippedPrompt As Label
+    Friend WithEvents txtUnitShipped As TextBox
+    Friend WithEvents lblRecord As Label
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents lblDay As Label
+End Class
---
+++ Lab1/Lab1/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class frmAverageUnitsShipped
+
+    Dim userInput As String = String.Empty
+    Dim num As Integer = 1
+    Dim iSum As Integer = 0
+    Dim dAverage As Double = 0
+    Private Sub frmAverageUnitsShipped_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblUnitShippedPrompt_Click(sender As Object, e As EventArgs) Handles lblUnitShippedPrompt.Click
+
+    End Sub
+
+    Private Sub txtUnitShipped_TextChanged(sender As Object, e As EventArgs) Handles txtUnitShipped.TextChanged
+
+    End Sub
+
+    Private Sub lblDay_Click(sender As Object, e As EventArgs) Handles lblDay.Click
+
+    End Sub
+
+    Private Sub lblRecord_Click(sender As Object, e As EventArgs) Handles lblRecord.Click
+
+    End Sub
+
+    Private Sub lblAverage_Click(sender As Object, e As EventArgs) Handles lblAverage.Click
+
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        If IsNumeric(txtUnitShipped.Text) Then
+            'If Double.TryParse(userInput, num) Then        'I tried making the user get an invalid msg if they inputed a decimal value but it didnt work 
+            'MessageBox.Show("ERROR. ")
+            'Else
+            iSum = iSum + CInt(txtUnitShipped.Text)
+                num = num + 1
+                lblDay.Text = "Day " & num.ToString
+                lblRecord.Text = lblRecord.Text & txtUnitShipped.Text & vbCrLf
+            'End If
+
+        Else
+            MessageBox.Show("ERROR. You had inputed an incorrect value. You can only input whole numbers. Please try again.")
+        End If
+
+            If (num = 8) Then
+                btnEnter.Enabled = False
+            dAverage = iSum / 7.0
+            dAverage = Math.Round(dAverage, 2)
+            lblDay.Text = "Day 7"
+                lblAverage.Text = "Average for this week: " & dAverage
+                MessageBox.Show("This week the average units you had sold was " & dAverage)
+            End If
+
+        txtUnitShipped.Text = ""
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        num = 1
+        iSum = 0
+        dAverage = 0
+        lblRecord.Text = " "
+        lblDay.Text = "Day " & num.ToString
+        lblAverage.Text = " "
+        txtUnitShipped.Clear()
+        btnEnter.Enabled = True
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+
+    End Sub
+
+End Class
